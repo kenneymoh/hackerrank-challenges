81c50f8e5add1b79bf6bef6a4685531bd09813f7
kennedy <kenneyrit@gmail.com>
2023-02-10 08:50:55 +0300

    done

--- enumarable_each_with_index.rb
+++ enumarable_each_with_index.rb
@@ -2,7 +2,7 @@ def skip_animals(animals, skip)
   # Your code here
         new_array=[]
     animals.each_with_index do | animal, index |
-        new_array << "#{index}:#{animal}" if index >= skip
+        new_array.push( "#{index}:#{animal}") if index >= skip
         end
     new_array
 end
